642d6534c0a633681d264637b3d71cee7ab32dbe
Salah Eddine Slaoui <sslaoui@e3r3p9.1337.ma>
2024-02-02 10:56:47 +0100

    Yo

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sslaoui <sslaoui@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/19 20:38:13 by sslaoui           #+#    #+#             */
+/*   Updated: 2023/11/25 03:28:26 by sslaoui          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*p;
+
+	if (!new || !lst)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	p = *lst;
+	while (p->next != NULL)
+		p = p->next;
+	p->next = new;
+}
